91caf4ab453732c23048142159fa37f0b665bc86
AmirA24-art <abduljelila7@gmail.com>
2025-05-11 07:12:08 -0700

    Update headerText.h

--- headerText/headerText.h
+++ headerText/headerText.h
@@ -1,1 +1,6 @@
+#ifndef HEADERTEXT_H // stands for "if not defined" used to check HEADERTEXT_H has not been defined yet.
+#define HEADERTEXT_H // if the HEADERTEXT_H has not been defined, it defines it.
 
+void header(); // function declaration 
+
+#endif // This ends the conditional started by #ifndef.
