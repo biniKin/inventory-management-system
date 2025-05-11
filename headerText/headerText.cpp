e3dfb628a79c7a196fa77c42569b6ff1067acdfe
Biniyam Kinfe <biniyamkinfe21@gmail.com>
2025-05-11 07:11:16 -0700

    create headerText.cpp folder

--- headerText/headerText.cpp
+++ headerText/headerText.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include "headerText.h" // Include the header file inorder to define the function
 
-using namespace std;
-
-void header(){
-    cout << "===============================================================" << endl;
-    cout << "         INVENTORY MANAGEMENT SYSTEM       " << endl;
-    cout << "===============================================================" << endl;
-    cout << endl;
-    cout << "Welcome to the Inventory Management System!" << endl;
-    cout << "---------------------------------------------------------------" << endl;
-}
