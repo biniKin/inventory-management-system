51b1554208ce83c1c7a1d493d2edd7032b699a95
AmirA24-art <abduljelila7@gmail.com>
2025-05-11 07:10:03 -0700

    Update auth.h

--- auth/auth.h
+++ auth/auth.h
@@ -1,1 +1,17 @@
 
+#ifndef AUTHUSER_H
+#define AUTHUSER_H
+
+#include <sqlite3.h>
+#include "../database/database.h"
+
+// Returns the role of the authenticated user
+UserRole authUser(sqlite3* db);
+
+// User registration
+void registerNewUser(sqlite3* db);
+
+// User management (admin only)
+void manageUsers(sqlite3* db);
+
+#endif
